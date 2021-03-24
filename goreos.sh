9e67f5c7583318b8f0bc4b806da07a28742bd162
lchryms45 <lchryms45@googlemail.com>
2021-03-24 17:29:34 +0000

    goreos

--- goreos.sh
+++ goreos.sh
@@ -46,14 +46,14 @@ for choice in $choices
 do
     case $choice in
 		1)
-            yes | sudo pacman -S xorg networkmanager openbox openbox-themes-extra obconf obkey-gtk3 obmenu-generator polybar lxappearance feh skippy-xd conky dunst rofi qt5-styleplugins terminus-font xcursor-breeze papirus-icon-theme lightdm-gtk-greeter-settings neofetch manjaro-settings-manager pamac arandr picom gparted htop xclip xsel gnome-disk-utility xfce4-power-manager git lxsession gtk2-perl perl-file-desktopentry nano wine-staging xarchiver pcmanfm p7zip termite micro mousepad viewnior galculator etcher scrot pavucontrol qjackctl jack2 brave gimp audacity vlc moc transmission-gtk uget simplescreenrecorder &&
+            sudo pacman -S xorg networkmanager openbox openbox-themes-extra obconf obkey-gtk3 obmenu-generator polybar lxappearance feh skippy-xd conky dunst rofi qt5-styleplugins terminus-font xcursor-breeze papirus-icon-theme lightdm-gtk-greeter-settings neofetch manjaro-settings-manager pamac arandr picom gparted htop xclip xsel gnome-disk-utility xfce4-power-manager git lxsession gtk2-perl perl-file-desktopentry nano wine-staging xarchiver pcmanfm p7zip termite micro mousepad viewnior galculator etcher scrot pavucontrol qjackctl jack2 brave gimp audacity vlc moc transmission-gtk uget simplescreenrecorder &&
             lxpolkit &&
-            yes | pamac build sweet-theme-git sweet-theme-dark beautyline dropbox dropbox-cli &&
+            pamac build sweet-theme-git sweet-theme-dark beautyline dropbox dropbox-cli &&
             ./goreos.sh
             ;;
 		2)
-            yes | sudo pacman -S inkscape kdenlive blender obs-studio steam discord handbrake nicotine+ soundconverter &&
-            yes | pamac build zoom franz-bin
+            sudo pacman -S inkscape kdenlive blender obs-studio steam discord handbrake nicotine+ soundconverter &&
+            pamac build zoom franz-bin
             ./goreos.sh
             ;;
 		3)
